2a40cf43b3cf6c2c21b5e50f1e5b978ce3795e68
olu <ebenezerajayi@gmail.com>
2020-10-25 13:22:29 +0100

    :zap: Logged In user Header

---
+++ frontend/src/Components/Components/HeaderBar/HeaderBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { HeaderDiv } from './HeaderBar.styles'
+
+const HeaderBar = () => (
+  <HeaderDiv>
+    <p> Sign out </p>
+    <p>Hello Olu</p>
+    <i class="fa fa-user-circle fa-3x" aria-hidden="true"></i>
+  </HeaderDiv>
+)
+export default HeaderBar;
---
+++ frontend/src/Components/Components/HeaderBar/HeaderBar.styles.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const HeaderDiv = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  height: 40px;
+  background-color: #fbfbfb;
+  padding: 7px 7.5%;
+  font-size:13px;
+  color: #383636;
+  p {
+    padding: 0 10px;
+    cursor: pointer;
+  }
+`;
+
+export {HeaderDiv}
